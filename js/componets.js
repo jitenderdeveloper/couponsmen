0d005fd80f313179622aeb6f9870d9196558a802
Jitender Developer <jitenderdeveloper0741@gmail.com>
2023-05-31 23:57:27 +0530

    couponsmen

---
+++ js/componets.js
@@ -0,0 +1,9 @@
+$(function () {
+  $("#header_load").load("components/header.html");
+  $("#home_load").load("components/home/home.html");
+  $("#head_load").load("components/head.html");
+  $("#category_load").load("components/home/category.html");
+  $("#filter_load").load("components/home/filterClient.html");
+  $("#footer_load").load("components/footer.html");
+  $("#ads_load").load("components/home/addsSlider.html");
+});
