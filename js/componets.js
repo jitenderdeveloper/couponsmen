9651cfcfc8c54aa000cb4353d3a1d2dc979d9adc
Jitender Developer <jitenderdeveloper0741@gmail.com>
2023-06-04 13:22:33 +0530

    updated

--- js/componets.js
+++ js/componets.js
@@ -7,4 +7,13 @@ $(function () {
   $("#footer_load").load("components/footer.html");
   $("#ads_load").load("components/home/addsSlider.html");
   $("#store_load").load("components/home/store.html");
+  $("#offer_load").load("components/home/offers.html");
+
+// ----------------------------stores--------------------------------------------
+  $("#store_banner_load").load("components/store/store-slider.html");
+  $("#filter_all_load").load("components/store/store-filter.html");
+  $("#coupons_load").load("components/store/coupons.html");
+  $("#deals_load").load("components/store/deals.html");
+  $("#product_load").load("components/store/product.html");
+  $("#exclusive_load").load("components/store/exclusive-coupons.html");
 });
